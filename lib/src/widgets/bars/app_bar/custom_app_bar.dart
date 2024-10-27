a93b4e1b78aa8ca002fe2cbf5e63b8dc2774fa73
Bahrican Yesil <bahricanyesil@gmail.com>
2024-10-28 01:37:20 +0300

    fix: update custom app bar implementation

--- lib/src/widgets/bars/app_bar/custom_app_bar.dart
+++ lib/src/widgets/bars/app_bar/custom_app_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_common_resources/src/extensions/context/context_extensions.dart';
-
-import '../../texts/base_text.dart';
+import 'package:flutter_common_resources/src/responsive/responsiveness_extensions.dart';
 
 /// A custom app bar widget.
 @immutable
@@ -9,36 +8,35 @@ class CustomAppBar extends AppBar {
   /// Creates a new custom app bar.
   CustomAppBar(
     BuildContext context, {
-    String? title,
-    List<Widget> actions = const <Widget>[],
-    TextStyle? titleTextStyle,
-    double? titleSpaceFactor,
+    Widget? leadingWidget,
+    Widget? centerWidget,
+    Widget? trailingWidget,
+    double horizontalMarginFactor = 4,
+    double spacingFactor = 4,
     super.key,
   }) : super(
-          title: _titleWidget(title, titleTextStyle, context),
-          titleSpacing: context.widthF * (titleSpaceFactor ?? hSpaceFactor),
-          centerTitle: false,
-          actions: actions,
-          backgroundColor: Colors.transparent,
-          surfaceTintColor: Colors.transparent,
           elevation: 0,
-          leadingWidth: context.widthF * 11,
+          backgroundColor: context.themeD.scaffoldBackgroundColor,
+          automaticallyImplyLeading: false,
+          toolbarHeight: kToolbarHeight + context.deviceH(1),
+          actions: <Widget>[
+            context.hBox(horizontalMarginFactor),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: leadingWidget ?? const SizedBox.expand(),
+              ),
+            ),
+            context.hBox(spacingFactor),
+            centerWidget ?? const SizedBox.shrink(),
+            context.hBox(spacingFactor),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: trailingWidget ?? const SizedBox.expand(),
+              ),
+            ),
+            context.hBox(horizontalMarginFactor),
+          ],
         );
-
-  static Text? _titleWidget(
-    String? title,
-    TextStyle? titleTextStyle,
-    BuildContext context,
-  ) {
-    if (title == null) return null;
-    return BaseText(
-      title,
-      textAlign: TextAlign.start,
-      style: titleTextStyle ??
-          context.displaySmall?.copyWith(fontWeight: FontWeight.bold),
-    );
-  }
-
-  /// Horizontal space factor.
-  static const double hSpaceFactor = 5;
 }
